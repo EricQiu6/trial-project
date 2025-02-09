535e13f8f23c5b9a9342fc3116eb06587754504f
Eric Qiu <ericq6666@gmail.com>
2025-02-09 14:46:52 -0500

    fixed all train data loading

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,6 +1,5 @@
 import os
 import h5py
-import torch
 from torch.utils.data import Dataset, DataLoader
 import fastmri
 from fastmri.data import transforms as T
@@ -28,8 +27,11 @@ class FastMRIClassificationDataset(Dataset):
             if not os.path.isdir(organ_dir):
                 raise ValueError(f"Directory not found: {organ_dir}")
 
-            candidate_dir = os.path.join(organ_dir, "multicoil-train")
-            target_dir = candidate_dir if os.path.isdir(candidate_dir) else organ_dir
+            candidate_dir = os.path.join(organ_dir, f"multicoil-{self.split}")
+            if not os.path.isdir(candidate_dir):
+                raise ValueError(f"Directory not found: {candidate_dir}")
+            
+            target_dir = candidate_dir
 
             for fname in os.listdir(target_dir):
                 if fname.endswith('.h5'):
@@ -38,18 +40,6 @@ class FastMRIClassificationDataset(Dataset):
 
     def __len__(self):
         return len(self.samples)
-
-    def center_crop(self, image, crop_size):
-        """Center crop a tensor image of shape [C, H, W]."""
-        c, h, w = image.shape
-        if isinstance(crop_size, int):
-            new_h, new_w = crop_size, crop_size
-        else:
-            new_h, new_w = crop_size
-
-        start_h = (h - new_h) // 2
-        start_w = (w - new_w) // 2
-        return image[:, start_h:start_h + new_h, start_w:start_w + new_w]
 
     def __getitem__(self, idx):
         file_path, label = self.samples[idx]
@@ -72,14 +62,9 @@ class FastMRIClassificationDataset(Dataset):
 
         # Apply center crop if specified
         if self.center_crop_size is not None:
-            image = self.center_crop(image, self.center_crop_size)
+            image = T.center_crop(image, self.center_crop_size)
 
-        sample = {'image': image, 'label': label}
-
-        if self.transform:
-            sample = self.transform(sample)
-
-        return sample
+        return image, label
 
 def main():
     print("Hello, FastMRI!")
@@ -89,13 +74,19 @@ def main():
         split='train',
         center_crop_size=(128, 128)
     )
+    val_dataset = FastMRIClassificationDataset(
+        root_dir="~/trial-project/data/", 
+        split='val',
+        center_crop_size=(128, 128)
+    )
+
     train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=2)
 
     print("Number of samples in train dataset:", len(train_dataset))
     for batch in train_loader:
-        images = batch['image']  # shape [B, 1, H, W]
-        labels = batch['label']
+        images, labels = batch  # shape [B, 1, H, W] and labels
         print("Batch images shape:", images.shape)
+        print("Batch images dtype:", images.dtype)
         print("Batch labels:", labels)
         break
 
